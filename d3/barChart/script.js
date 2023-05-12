dba9d8b48ce88616d93ebe3f9993822c35ae92e2
Paul van der Lei <Paul.vanderlei@wics.nl>
2023-05-12 11:23:17 +0200

    added correct deps

--- d3/barChart/script.js
+++ d3/barChart/script.js
@@ -1,1 +1,13 @@
-alert("hey");
+const projectName = "bar-chart";
+
+const width = 800;
+const height = 400;
+const barWidth = width / 275;
+
+d3.json(
+  "https://raw.githubusercontent.com/FreeCodeCamp/ProjectReferenceData/master/GDP-data.json"
+)
+  .then((data) => {
+    console.log("🚀 ~ file: script.js ~ line 11 ~ data", data);
+  })
+  .catch((e) => console.log("❗️ Error: ", e.message));
